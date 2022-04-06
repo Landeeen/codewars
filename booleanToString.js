49da10ee758f4a5f02d1e1397eaae95f85432629
Samu <samu.landen@gmail.com>
2022-04-06 22:16:11 +0200

    more cw

---
+++ booleanToString.js
@@ -0,0 +1,11 @@
+function boolToWord(bool) {
+    if(bool === true){
+        return 'Yes'
+      } else if(bool === false) {
+        return 'No'
+      }
+}
+
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
---
+++ oppositeNum.js
@@ -0,0 +1,3 @@
+function opposite() {
+    return -number
+}
