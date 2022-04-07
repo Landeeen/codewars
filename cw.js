f2bfa2085cb244bb7d17db18e3a36b4b665ad148
Samu <samu.landen@gmail.com>
2022-04-07 16:55:42 +0200

    april 7 cw

---
+++ cw.js
@@ -0,0 +1,13 @@
+// return reverse string
+
+function solution(str){
+    return str.split("").reverse().join("");
+}
+
+// return sum of positives
+
+function positiveSum(arr) {
+  arr.forEach(element => { if(element > 0){element += }
+      
+  });
+}
